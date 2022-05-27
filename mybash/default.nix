9a09c1a50f0c0fdf18d8329199fda27629a918ee
Thomas <thomas@mcth.fr>
2022-05-27 21:37:29 +0200

    Update without scripts

--- mybash/default.nix
+++ mybash/default.nix
@@ -4,19 +4,22 @@ pkgs.stdenv.mkDerivation rec {
   pname = "mybash";
   version = "0.1.0";
 
-  src = ./.;
+  src = builtins.fetchGit {
+    url = "git@gitlab.mcth.fr:thomas/awesome-script.git";
+    rev = "f20cff4231c58c674d5b03794a3f6ae625204d98";
+  };
 
   buildInputs = [
     pkgs.bash
   ];
 
   configurePhase = ''
-    cat urlencode
+    ls -l 
   '';
 
   installPhase = ''
     mkdir -p $out/bin
-    mv switchvault $out/bin
-    mv urlencode $out/bin
+    mv bash/urlencode $out/bin/
+    mv bash/switchvault $out/bin/
   '';
 }

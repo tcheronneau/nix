071f17b7fe3b206e46a14f3bea599594b96bc2dc
Thomas <thomas.cheronneau@luxhub.com>
2022-06-29 11:28:36 +0200

    Update neovim

--- nvim/default.nix
+++ nvim/default.nix
@@ -130,7 +130,7 @@ pkgs.neovim.override {
         
         nnoremap <silent> <C-w><C-t> :sp<CR>:resize 10<CR>:Neomux<CR>
         
-        autocmd VimEnter * call StartUp()
+        "autocmd VimEnter * call StartUp()
         let g:chromatica#libclang_path='/usr/lib/llvm-11/lib/'
         let g:chromatica#enable_at_startup=1
         " Grep not opening file

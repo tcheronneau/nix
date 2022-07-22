319f54ed259a22cb9be21a2bbb163a40b7e08bd4
Thomas <thomas@mcth.fr>
2022-07-22 12:38:49 +0200

    update

--- default.nix
+++ default.nix
@@ -4,5 +4,6 @@ rec {
     scripts = pkgs.callPackage ./mybash {};
     enpass = pkgs.callPackage ./enpass {};
     sonarr = pkgs.callPackage ./sonarr {};
+    seafile = pkgs.callPackage ./seafile {};
     docker-sonarr = pkgs.callPackage ./sonarr/docker.nix {};
 }

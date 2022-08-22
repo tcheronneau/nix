40482f9a9fd100cd39256f7f8eee9b45703bb0b5
Thomas <thomas.cheronneau@luxhub.com>
2022-08-22 16:31:01 +0200

    Update nvim

--- sonarr/docker.nix
+++ sonarr/docker.nix
@@ -2,9 +2,9 @@
 let sonarr = pkgs.callPackage ./default.nix {};
 in 
   pkgs.dockerTools.buildLayeredImage {
-    name = "hub.mcth.fr/sonarr.nix";
+    name = "registry.mcth.fr/docker/sonarr";
     contents = [ sonarr ];
-    tag = "latest";
+    tag = "nix";
     created = "now";
     config = {
       Cmd = [

a89fabd79b01b4786ac9a7eb6cabbc46b92e35cf
Thomas <thomas@mcth.fr>
2022-09-01 22:52:46 +0200

    Testing almost ok

--- sonarr/test.nix
+++ sonarr/test.nix
@@ -1,2 +1,10 @@
-{ pkgs }:
-let sonarr = import ./docker.nix
+with import <nixpkgs> {} ;
+let 
+  testing = pkgs.writeShellScriptBin "dgoss" ''
+    ${pkgs.dgoss}/bin/dgoss run --pull=always registry.mcth.fr/docker/sonarr:latest
+  '';
+in 
+stdenv.mkDerivation rec {
+  name = "test-environment";
+  buildInputs = [ testing ];
+}

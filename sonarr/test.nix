17642e5b710a3bfb8993e1dc9732414f09947497
Thomas <thomas.cheronneau@luxhub.com>
2022-09-02 08:54:41 +0200

    Update better way

--- sonarr/test.nix
+++ sonarr/test.nix
@@ -1,10 +1,13 @@
 with import <nixpkgs> {} ;
 let 
   testing = pkgs.writeShellScriptBin "dgoss" ''
-    ${pkgs.dgoss}/bin/dgoss run --pull=always registry.mcth.fr/docker/sonarr:latest
+    ${pkgs.dgoss}/bin/dgoss run registry.mcth.fr/docker/sonarr:latest
   '';
 in 
-stdenv.mkDerivation rec {
-  name = "test-environment";
+#stdenv.mkDerivation rec {
+  #name = "test-environment";
+  #buildInputs = [ testing ];
+pkgs.mkShell rec { 
   buildInputs = [ testing ];
+  shellHook = "dgoss";
 }

8c60f94799c38b032d4be6eab6468ecc61493236
Thomas <thomas.cheronneau@luxhub.com>
2022-09-09 13:49:07 +0200

    Testing again

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
-{ pkgs ? import (builtins.fetchGit { name = "unstable"; url = "https://github.com/NixOS/nixpkgs"; rev = "21de2b973f9fee595a7a1ac4693efff791245c34";})> {} }:
+#{ pkgs ? import (builtins.fetchGit { name = "unstable"; url = "https://github.com/NixOS/nixpkgs"; rev = "21de2b973f9fee595a7a1ac4693efff791245c34";})> {} }:
+{ pkgs ? import <nixpkgs-unstable> {} }:
 rec {
     nvim = pkgs.callPackage ./nvim {};
     scripts = pkgs.callPackage ./mybash {};

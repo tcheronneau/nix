c7b97375ecea7fdd815298c7c17065d49dd0005d
thomas <thomas.cheronneau@luxhub.com>
2022-10-10 11:42:50 +0000

    Add cd in script

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ podman image trust set --type accept registry.mcth.fr
 
 echo -n $CI_REGISTRY_PASSWORD | podman login -u $CI_REGISTRY_USER --password-stdin $CI_REGISTRY
 
+cd ${NAME}
 podman load < $(nix-build -E 'with import <nixos-unstable> {}; callPackage ./docker.nix {}')
 podman tag ${CI_REGISTRY_IMAGE}:nix ${CI_REGISTRY_IMAGE}:${VERSION}
 podman push ${CI_REGISTRY_IMAGE}:${VERSION}

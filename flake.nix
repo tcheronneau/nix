f8c8ae7db20dcfbc28fac7bcced03def9383b33e
Thomas <thomas@mcth.fr>
2022-10-31 21:50:31 +0100

    Trying with compatibility

--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     #flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
   outputs = { self, ... } @inputs : {
-    packages.x86_64-linux = (import ./default.nix inputs);
+    packages.x86_64-linux = (import ./packages.nix inputs);
   };
 }

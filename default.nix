98701acc44496aa9ee2857f283236f171b2ddd43
Thomas <thomas@mcth.fr>
2022-10-31 22:07:20 +0100

    Should all work

--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
-(import
-  (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  { src = ./.; }
-).defaultNix
+{ pkgs? import ./source.nix {} }:
+#{ callPackage }:
+with pkgs;
+rec {
+    nvim = callPackage ./nvim {};
+    scripts = callPackage ./mybash {};
+    enpass = callPackage ./enpass {};
+    sonarr = callPackage ./sonarr {};
+    radarr = callPackage ./radarr {};
+    jackett = callPackage ./jackett {};
+    bazarr = callPackage ./bazarr {};
+    tautulli = python3Packages.callPackage ./tautulli {};
+    plex = callPackage ./plex {};
+    seafile = callPackage ./seafile {};
+    kubernetes = callPackage ./kubernetes {};
+    docker-sonarr = callPackage ./sonarr/docker.nix {};
+    docker-jackett = callPackage ./jackett/docker.nix {};
+}
+

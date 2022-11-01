cc18df59aa3d42c64372ad3460b7869d90d84697
Abott <abott@mcth.fr>
2022-11-01 07:17:29 +0100

    Releasing new version 0.20.2182 for jackett [skip ci]

--- jackett/default.nix
+++ jackett/default.nix
@@ -9,13 +9,13 @@
 
 buildDotnetModule rec {
   pname = "jackett";
-  version = "0.20.2180";
+  version = "0.20.2182";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "AwkWRFYHNnrnT2W1M6hDMYt+7dqzTVnLjtRrSDfkWNw=";
+    sha256 = "K2U9Nauv8bD8xAlqR/Hh+KW5WlvozaJV9pdD8Y/j7w0=";
   };
 
   projectFile = "src/Jackett.Server/Jackett.Server.csproj";

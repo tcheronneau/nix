598e75da50757ba319ace207473f6ae83670bb3b
Thomas <thomas.cheronneau@luxhub.com>
2023-02-02 14:45:04 +0100

    Update plex with new version :

--- flaresolverr.nix
+++ flaresolverr.nix
@@ -1,6 +1,21 @@
-{ lib, fetchFromGitHub, buildPythonPackage, python3, wrapPython, makeWrapper, chromedriver, chromium, xvfb-run }:
+{ lib, fetchFromGitHub, buildPythonPackage, python3, python3Packages, wrapPython, makeWrapper, chromedriver, chromium, xvfb-run }:
 let 
-  pydep = with python3.pkgs; [
+  python = let 
+    packageOverrides = prev: final: {
+      selenium = final.selenium.overridePythonAttrs (old: {
+        src = fetchFromGitHub {
+          owner = "SeleniumHQ";
+          repo = "selenium";
+          rev = "refs/tags/selenium-4.8.0";
+          hash = "sha256-YTi6SNtTWuEPlQ3PTeis9osvtnWmZ7SRQbne9fefdco=";
+        };
+        postInstall = ''
+          install -Dm 755 ../rb/lib/selenium/webdriver/atoms/getAttribute.js $out/${python3Packages.python.sitePackages}/selenium/webdriver/remote/getAttribute.js
+          install -Dm 755 ../rb/lib/selenium/webdriver/atoms/isDisplayed.js $out/${python3Packages.python.sitePackages}/selenium/webdriver/remote/isDisplayed.js
+        '';
+      });
+    }; in python3.override { inherit packageOverrides; };
+  pydep = with python.pkgs; [
     bottle
     waitress
     selenium
@@ -25,7 +40,7 @@ buildPythonPackage rec {
   version = "3.0.2";
   format = "other";
 
-  pythonPath = [ python3 ];
+  pythonPath = [ python ];
   nativeBuildInputs = [ wrapPython makeWrapper ] ++ extrainputs ;
   propagatedBuildInputs = pyinput ++ extrainputs; 
 
@@ -40,7 +55,7 @@ buildPythonPackage rec {
     runHook preInstall
     mkdir -p $out/src
     cp -r . $out/src
-    makeWrapper "${python3}/bin/python" $out/bin/flaresolverr \
+    makeWrapper "${python}/bin/python" $out/bin/flaresolverr \
       --add-flags "$out/src/src/flaresolverr.py" \
       --prefix PYTHONPATH : "$PYTHONPATH" \
       --prefix PYTHONPATH : "${lib.makeBinPath [chromium chromedriver xvfb-run]}" \
--- plex/raw.nix
+++ plex/raw.nix
@@ -12,16 +12,16 @@
 # server, and the FHS userenv and corresponding NixOS module should
 # automatically pick up the changes.
 stdenv.mkDerivation rec {
-  version = "1.30.2.6563-3d4dc0cce";
+  version = "1.31.0.6654-02189b09f";
   pname = "plexmediaserver";
 
   # Fetch the source
   src = if stdenv.hostPlatform.system == "aarch64-linux" then fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_arm64.deb";
-    hash = "sha256-fGZYfHw2XeTIAJJX4T0KgS4y+rGR70wtC5A6ggjr5ms=";
+    hash = "sha256-ttkvYD+ALxfZpQutI1VyTbmQi/7hmvZ+YMUv3lskeWU=";
   } else fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-    hash = "sha256-8VUkeRxpmBXDP4MhWyKp6GEnK29L+Oh12s/bpId3vXo=";
+    hash = "sha256-TTEcyIBFiuJTNHeJ9wu+4o2ol72oCvM9FdDPC83J3Mc=";
   };
 
   outputs = [ "out" "basedb" ];

982da0d0f009200748489f887f83d1f81422d21b
otto <otto@mcth.fr>
2023-03-24 22:36:33 +0100

    Update plex with new version :

--- manage.sh
+++ manage.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env nix-shell 
-#!nix-shell -i bash -p docker
-
+DIR=$(basename -- "$0";)
+echo $DIR
 if [ -z $1 ]
 then
   SOFTS=( "plex" "sonarr" "radarr" "tautulli" "jackett" "prowlarr" )
@@ -19,14 +18,14 @@ do
   echo ""
   echo "------------------------------"
   echo ""
-  VERSION=$(./${SOFT}/update.sh|awk '/VERSION/ {print $2;}')
+  VERSION=$(${DIR}/${SOFT}/update.sh|awk '/VERSION/ {print $2;}')
   if [[ `git status --porcelain` ]]
   then
     if [ -f "${SOFT}/docker.nix" ]
     then 
       echo "Building docker for ${SOFT} on version: ${VERSION}"
       nix build .\#docker-${SOFT}
-      docker load < $(readlink ./result)
+      docker load < $(readlink ${DIR}/result)
       docker tag mcth/${SOFT}:nix mcth/${SOFT}:latest
       docker tag mcth/${SOFT}:nix mcth/${SOFT}:${VERSION}
       docker push mcth/${SOFT}

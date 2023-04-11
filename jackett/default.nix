70b1dd0d9c2ae5a611484113d39ef40f90621ff9
otto <otto@mcth.fr>
2023-04-12 00:03:50 +0200

    Update jackett with new version : 0.20.3870

--- jackett/default.nix
+++ jackett/default.nix
@@ -9,13 +9,13 @@
 
 buildDotnetModule rec {
   pname = "jackett";
-  version = "0.20.3861";
+  version = "0.20.3870";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "wTZSW3cBnPQPKa+H7MeCIZdBnZs+sl2uphs3JB8rAUo=";
+    sha256 = "+S/CNX5T7GPZF3ieZ8ifVHO78xgxG0DfLB4musXK3+Y=";
   };
 
   projectFile = "src/Jackett.Server/Jackett.Server.csproj";

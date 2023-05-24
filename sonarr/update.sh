92f592600042848936e0fe1afb8c2438e976a6ee
Thomas <thomas@mcth.fr>
2023-05-24 11:09:47 +0200

    Update sonarr with new version : 3.0.10.1567

--- sonarr/update.sh
+++ sonarr/update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl jq common-updater-scripts
 
-LAST=$(curl -s https://api.github.com/repos/sonarr/sonarr/tags|jq -r '.[0].name'|sed "s/v//g")
+LAST=$(curl -s https://services.sonarr.tv/v1/download/main|jq -r '.version')
+#LAST=$(curl -s https://api.github.com/repos/sonarr/sonarr/tags|jq -r '.[0].name'|sed "s/v//g")
 VERSION=${VERSION:-$LAST}
 update-source-version sonarr "$VERSION"
 echo "VERSION $VERSION"

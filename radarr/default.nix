1517ea77a79c67ff882e970026d6844f4616a11a
otto <otto@mcth.fr>
2023-05-29 00:00:39 +0200

    Update radarr with new version :

--- radarr/default.nix
+++ radarr/default.nix
@@ -16,7 +16,7 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "radarr";
-  version = "4.5.2.7318";
+  version = "4.5.2.7388";
 
   src = fetchurl {
     url = "https://github.com/Radarr/Radarr/releases/download/v${version}/Radarr.develop.${version}.${os}-core-${arch}.tar.gz";

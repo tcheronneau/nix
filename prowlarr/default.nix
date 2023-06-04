a3dea48c7c45d47005c69c997b9b65267a3ef513
otto <otto@mcth.fr>
2023-06-05 00:03:11 +0200

    Update prowlarr with new version :

--- prowlarr/default.nix
+++ prowlarr/default.nix
@@ -23,7 +23,7 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "prowlarr";
-  version = "1.5.1.3422";
+  version = "1.5.2.3484";
 
   src = fetchurl {
     url = "https://github.com/Prowlarr/Prowlarr/releases/download/v${version}/Prowlarr.master.${version}.${os}-core-${arch}.tar.gz";

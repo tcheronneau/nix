df79c7531040c2500612a970c88ce02fef74dec8
otto <otto@mcth.fr>
2023-07-03 00:01:26 +0200

    Update prowlarr with new version : 1.7.0.3623

--- prowlarr/default.nix
+++ prowlarr/default.nix
@@ -16,14 +16,14 @@ let
   }."${stdenv.hostPlatform.system}" or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
   hash = {
-    x64-linux_hash = "sha256-PMAfz9Fgr/SBlwQ5TB//NTqfTUfxL+Zd+uJYjbWRsiQ=";
-    arm64-linux_hash = "sha256-jvRTlKsxo/Oa0TO6EeFwmhVAV9SE8Kgy3Y8kS1eIbA0=";
-    x64-osx_hash = "sha256-FBfDOQvN+8YmOYq69WrzZdwmcalkUz2hoiBmm01bvdw=";
+    x64-linux_hash = "sha256-anL2yBYlSpC/Pv9EFqW38ZQa34+If46QsTjPR7sV+C8=";
+    arm64-linux_hash = "sha256-Me7SU5iom7+Cru8xfDDm5rfpbygrO4ZDJgbq/tzP9dA=";
+    x64-osx_hash = "sha256-LO5uqYuLS3mxw9gXQEuDk/mIL7KXeRJBa7/WfFmavqQ=";
   }."${arch}-${os}_hash";
 
 in stdenv.mkDerivation rec {
   pname = "prowlarr";
-  version = "1.6.3.3608";
+  version = "1.7.0.3623";
 
   src = fetchurl {
     url = "https://github.com/Prowlarr/Prowlarr/releases/download/v${version}/Prowlarr.master.${version}.${os}-core-${arch}.tar.gz";

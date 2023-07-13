8cb9f28fdd234bbf756ef0000f9c49ad11c8099c
Thomas <thomas@mcth.fr>
2023-07-13 11:32:05 +0200

    New nvim config

--- nvim/default.nix
+++ nvim/default.nix
@@ -10,34 +10,13 @@ let
       sha256 = "sha256-6Gr6/soyN5r+NRpDrFs9aT/assuQF9ydR3TfZnPlygI=";
     };
   };
-  coc-ansible = vimUtils.buildVimPlugin {
-    pname = "coc-ansible";
-    version = "test";
-    src = fetchFromGitHub {
-      owner = "yaegassy";
-      repo = "coc-ansible";
-      rev = "507cde4d172e1732266b4d50d2a98dfde3dc6ab9";
-      sha256 = "sha256-nFwaDcWB5zkdb9MZLdKRh7dQS5vxdQ0W7muKfPcKvwc=";
-    };
-  };
-  blamer = vimUtils.buildVimPlugin {
-    pname = "blamer";
-    version = "test";
-    src = fetchFromGitHub {
-      owner = "APZelos";
-      repo = "blamer.nvim";
-      rev = "f4eb22a9013642c411725fdda945ae45f8d93181";
-      sha256 = "sha256-etLCmzOMi7xjYc43ZBqjPnj2gqrrSbmtcKdw6eZT8rM=";
-    };
-  };
   vimrc = builtins.readFile ./vimrc;
 in
 neovim.override {
   configure = {
     customRC = vimrc; 
     packages.myVimPackage = with vimPlugins; {
-      # see examples below how to use custom packages
-      start = [ blamer vim-one vim-airline vim-airline-themes fugitive vim-devicons salt-vim vim-terraform neomux rust-vim salt-vim vim-devicons vim-nix ansible-vim vim-go syntastic fzf-vim bufferline-nvim vim-nix vim-nixhash nvim-web-devicons nvim-tree-lua rust-tools-nvim telescope-nvim mason-nvim nvim-lspconfig mason-lspconfig-nvim cmp-nvim-lsp nvim-treesitter nvim-cmp ];
+      start = [ blamer-nvim vim-one vim-airline vim-airline-themes fugitive vim-devicons salt-vim vim-terraform neomux rust-vim salt-vim vim-devicons vim-nix ansible-vim vim-go syntastic fzf-vim bufferline-nvim vim-nix vim-nixhash nvim-web-devicons nvim-tree-lua rust-tools-nvim telescope-nvim mason-nvim nvim-lspconfig mason-lspconfig-nvim cmp-nvim-lsp nvim-treesitter nvim-cmp luasnip cmp-buffer neodev-nvim lualine-nvim gitsigns-nvim ];
       opt = [ ];
     };
   };

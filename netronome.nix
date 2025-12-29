ec4bce9d83a8aff8a7ff0839af34486c574cce21
Otto <otto@mcth.fr>
2025-12-29 15:11:30 +0100

    Working netronome

--- netronome.nix
+++ netronome.nix
@@ -14,9 +14,9 @@ stdenv.mkDerivation {
   inherit version;
 
   src = fetchurl {
-    url = "https://github.com/autobrr/netronome/releases/download/v${version}/netronome_${version}_linux_amd64.tar.gz";
+    url = "https://github.com/autobrr/netronome/releases/download/v${version}/netronome_${version}_linux_x86_64.tar.gz";
     # Run once to get hash
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-2uphJJEjrlLq0tji8PX7OsRp6ajo6nAlYV1Z4Ea3E+U=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];

9963aa50ffeb1cc906c330fe9c9dddfaa6093745
Otto <otto@mcth.fr>
2026-08-04 00:00:12 +0200

    Update plex with new version : 1.43.3.10861-07dfddaeb

--- plex/raw.nix
+++ plex/raw.nix
@@ -12,16 +12,16 @@
 # server, and the FHS userenv and corresponding NixOS module should
 # automatically pick up the changes.
 stdenv.mkDerivation rec {
-  version = "1.43.3.10828-00f62d37d";
+  version = "1.43.3.10861-07dfddaeb";
   pname = "plexmediaserver";
 
   # Fetch the source
   src = if stdenv.hostPlatform.system == "aarch64-linux" then fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_arm64.deb";
-    hash = "sha256-8IwK6FDjMfqCBOmqiCJlmNjakYGukfl/4B92lkupYqA=";
+    hash = "sha256-LUAGEQ5VzTkgyC2K7IPbIblD1XQwqU20hv+9AZAvFa4=";
   } else fetchurl {
     url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-    hash = "sha256-ieU0/7Vlrs2tsR1QhD2Cyk/pia4MfmAugx0Ec6Ook20=";
+    hash = "sha256-s8OpELTLfdincYQZawp76rsZx5AQXMR6+algH/Ev0zI=";
   };
 
   outputs = [ "out" "basedb" ];
